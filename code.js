4e7353658fe89c25eecc18fc00f8bf0e81b00477
Mahipalkhowal <khowalmahipal2001@gmail.com>
2022-11-30 14:45:19 +0530

    code_wrote

---
+++ code.js
@@ -0,0 +1,25 @@
+1.Prime Number
+
+num=13;
+for(i=0;i<num;i++){
+    if(num%i==0){
+	c++;
+}
+}
+if(c=2){
+    console.log("prime")
+}else{
+    console.log("not prime")
+}
+
+2.Palindrome Problem
+
+a="naman"
+b="";
+for(i=N;i>=0;i--){
+    b+=a[i];
+}
+if(b==a){
+    console.log("Palindrome")
+}else{
+    console.log("not palindrome")
