73186c33f2d55a698dc2692c5394140f45ded6e5
Parshya <prasadkadam7743@gmail.com>
2022-12-01 11:49:49 +0530

    All bugs are fixed

--- code.js
+++ code.js
@@ -1,19 +1,20 @@
 1.Prime Number
 
 num=13;
+c=0;
 for(i=0;i<num;i++){
     if(num%i==0){
 	c++;
 }
 }
-if(c=2){
+if(c==2){
     console.log("prime")
 }else{
     console.log("not prime")
 }
 
 2.Palindrome Problem
-
+N=5;
 a="naman"
 b="";
 for(i=N;i>=0;i--){
